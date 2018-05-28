346816d7729453c6a6aa1f2b9e3910ae6bc40699
Cangnan Dai <dcl_1016@outlook.com>
2018-05-28 23:25:31 +0800

    Day Two 参数传递和返回值

--- complex/complex-test.cpp
+++ complex/complex-test.cpp
@@ -6,11 +6,16 @@ int main()
 {
 	complex c1(2.5, 1.5);
 	complex c2;
-	complex* p = new complex(4);
-	cout << "实部:" << c1.real() << "\t虚部:" << c1.imag() << endl;
-	cout << "实部:" << c2.real() << "\t虚部:" << c2.imag() << endl;
-	cout << "实部:" << p->real() << "\t虚部:" << p->imag() << endl;
-	cout << "hejkd" << endl;
+	const complex* p = new complex(4);
+	cout << "c1实部:" << c1.real() << "\t虚部:" << c1.imag() << endl;
+	cout << "c2实部:" << c2.real() << "\t虚部:" << c2.imag() << endl;
+	cout << "p实部:" << p->real() << "\t虚部:" << p->imag() << endl;
+	cout << "==============c2.func(c1)================"<<endl;
+	c2.func(c1);
+	cout << "c2 两者相加："<<c2.func(c1) << endl;
+	cout << "============实现operator+=================" << endl;
+	c1.operator+=(*p);
+	cout << "c1实部:" << c1.real() << "\t虚部:" << c1.imag() << endl;
 	system("pause");
 	return 0;
 }
--- complex/complex.h
+++ complex/complex.h
@@ -14,18 +14,42 @@ class complex //class head
 {
 	//class body
 public:
-	complex(double r = 0, double i = 0 /*默认实参*/)
+	complex(double r = 0, double i = 0 /*默认实参，传值pass by value*/)
 		:re(r), im(i)//初始序列 等同于 re = r , im = i;
 	{ }//构造函数
-	complex& operator += (const complex&);
+	complex& operator += (const complex& /*传引用 pass by reference （to const）*/);
+	//返回值的传递也尽量传递引用（pass by reference）
 	double real() const { return re; }
 	double imag() const { return im; }
+	//一个类里面的函数可以分为可以改变数据和不会改变数据两种函数
+	//不会改变数据内容的函数加上const（important）
+	//在调用常量（const）时。类中没有标明方法没有标明const会出错
 
+	int func(const complex& param)
+	{
+		return param.re + param.im;
+	}//相同class的各个objects互为友元
 private:
 	double re, im;
 
-	friend complex& __doapl(complex*, const complex&);
+	friend/*friend 表示友元*/ complex& __doapl(complex*, const complex&);
+
+
 };
+
+inline complex&
+__doapl(complex *ths, const complex& r)
+{
+	ths->re += r.re;
+	ths->im += r.im;
+	return *ths;
+}
+inline complex&
+complex::operator +=(const complex& r)
+{
+	return __doapl(this, r);
+}
+
 
 
 //类---定义
